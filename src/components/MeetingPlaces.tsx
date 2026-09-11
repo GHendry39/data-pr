2d5679f6308bbc0e9e2eba8363e2616595345efe
Graeme <graeme@ogmorevalleywebsites.co.uk>
2026-09-11 19:23:58 +0100

    MeetingPlaces component debugged

--- src/components/MeetingPlaces.tsx
+++ src/components/MeetingPlaces.tsx
@@ -4,18 +4,19 @@ import { useEffect, useState } from 'react';
 
 type MeetingPlaceType = Schema['MeetingPlace']['type'];
 
-export function MeetingPlaces() {
-  const placesClient = generateClient<Schema>().models.MeetingPlace;
+const placesClient = generateClient<Schema>().models.MeetingPlace;
 
+export function MeetingPlaces() {
   const [meetingPlaces, setMeetingPlaces] = useState<Array<MeetingPlaceType>>(
     [],
   );
 
   useEffect(() => {
-    placesClient.observeQuery().subscribe({
+    const sub = placesClient.observeQuery().subscribe({
       next: data => setMeetingPlaces([...data.items]),
     });
-  }, [placesClient]);
+    return () => sub.unsubscribe();
+  }, []);
 
   function createPlace() {
     placesClient.create({
